d70aba2b298173ae89659e7d65209304c6526567
Wai <cloudgitcollab@gmail.com>
2024-09-22 19:19:03 +0000

    updated

--- tfc-create-hvn-vpc-peering/versions.tf
+++ tfc-create-hvn-vpc-peering/versions.tf
@@ -14,7 +14,7 @@ terraform {
 provider "aws" {
   region = var.aws_region
   access_key = data.vault_aws_access_credentials.master_iamadmin_creds.access_key
-  secret_key = data.vault_aws_access_credentials.master_netadmin_creds.secret_key
+  secret_key = data.vault_aws_access_credentials.master_iamadmin_creds.secret_key
 }
 
 # provider "vault" {
